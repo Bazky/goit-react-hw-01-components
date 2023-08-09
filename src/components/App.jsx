1fbcd6649c89f5ea91727dac4c407df8e777025e
Adrian Nastał <adriannastal9@gmail.com>
2023-08-09 21:52:10 +0200

    statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Profile } from './user/Profile';
+import { Statistics } from './stats/Statistics';
+import data from './stats/data.json';
 
 export const App = () => {
-  return <Profile />;
+  return (
+    <>
+      <Profile />
+      <Statistics title="UPLOAD STATS" stats={data} />
+    </>
+  );
 };
