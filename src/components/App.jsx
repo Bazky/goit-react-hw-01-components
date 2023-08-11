9d4a83f84c2cc96c2199d7c067005af31061a697
Adrian Nastał <adriannastal9@gmail.com>
2023-08-11 20:11:26 +0200

    friendlist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Profile } from './user/Profile';
 import { Statistics } from './stats/Statistics';
+import { FriendList } from './friends/FriendList';
 import data from './stats/data.json';
+import friends from './friends/friends.json';
 
 export const App = () => {
   return (
     <>
       <Profile />
       <Statistics title="UPLOAD STATS" stats={data} />
+      <FriendList friends={friends} />
     </>
   );
 };
---
+++ src/components/friends/FriendList.jsx
@@ -0,0 +1,25 @@
+import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendlist}>
+      {friends.map(friend => (
+        <li className={css.item} key={friend.id}>
+          {friend.isOnline ? (
+            <span className={css.online}></span>
+          ) : (
+            <span className={css.offline}></span>
+          )}
+
+          <img
+            className={css.avatar}
+            src={friend.avatar}
+            alt="User avatar"
+            width="48"
+          />
+          <p className={css.name}>{friend.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
