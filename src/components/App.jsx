a67332f5d321636117f3f8aca1dd95ec09e446d0
Adrian Nastał <adriannastal9@gmail.com>
2023-08-15 20:27:15 +0200

    fix2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,11 +12,13 @@ export const App = () => {
   return (
     <>
       <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
+        user={{
+          username: user.username,
+          tag: user.tag,
+          location: user.location,
+          avatar: user.avatar,
+          stats: user.stats,
+        }}
       />
       <Statistics title={'UPLOAD STATS'} stats={data} />
       <FriendList friends={friends} />
